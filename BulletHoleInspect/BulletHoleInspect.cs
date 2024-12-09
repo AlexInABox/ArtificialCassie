4d0fc2a2b728dc7d20017bc5a932b88109aef453
alexinabox <main@alexinabox.de>
2024-12-09 13:40:03 +0100

    added cassie event

--- BulletHoleInspect/BulletHoleInspect.cs
+++ BulletHoleInspect/BulletHoleInspect.cs
@@ -4,11 +4,13 @@
     using Exiled.API.Enums;
     using Exiled.API.Features;
     using Exiled.Events;
+    using Events;
 
     public class BulletHoleInspect : Plugin<Config>
     {
         private static BulletHoleInspect Singleton;
         public static BulletHoleInspect Instance => Singleton;
+        private CassieHandler cassieHandler;
         public override PluginPriority Priority { get; } = PluginPriority.Last;
 
         public override string Author { get; } = "AlexInABox";
@@ -17,6 +19,8 @@
         {
             Singleton = this;
             Log.Info("BulletHoleInspect has been enabled!");
+            cassieHandler = new CassieHandler();
+            Exiled.Events.Handlers.Cassie.SendingCassieMessage += cassieHandler.OnSendingCassieMessage;
 
             base.OnEnabled();
         }
@@ -25,6 +29,7 @@
         {
             Log.Info("BulletHoleInspect has been disabled!");
 
+            Exiled.Events.Handlers.Cassie.SendingCassieMessage -= cassieHandler.OnSendingCassieMessage;
             base.OnDisabled();
         }
     }
---
+++ BulletHoleInspect/Events/CassieEvent.cs
@@ -0,0 +1,23 @@
+namespace BulletHoleInspect.Events
+{
+    using System.Collections.Generic;
+
+    using Exiled.API.Features;
+    using Exiled.API.Features.Items;
+    using Exiled.Events.EventArgs.Cassie;
+
+
+    internal sealed class PlayerHandler
+    {
+
+        public void OnSendingCassieMessage(SendingCassieMessageEventArgs ev)
+        {
+            Log.Info("CASSIE Announcement:");
+            Log.Info($"Words: {ev.Words}");
+            Log.Info($"MakeHold: {ev.MakeHold}");
+            Log.Info($"MakeNoise: {ev.MakeNoise}");
+            Log.Info($"IsAllowed: {ev.IsAllowed}");
+
+        }
+    }
+}
