3ff263cc0fd6d55fe7c44e70c19e9ce6748e6adc
Alexander Betke <main@alexinabox.de>
2024-12-09 23:26:01 +0100

    system exception???

--- BulletHoleInspect/Events/CassieEvent.cs
+++ BulletHoleInspect/Events/CassieEvent.cs
@@ -21,7 +21,7 @@ namespace BulletHoleInspect.Events
             {
                 AudioController.SpawnDummy(99, "AudioPlayer BOT", "orange", "C.A.S.S.I.E");
             }
-            catch (Exception e)
+            catch (System.Exception)
             {
                 // Handle exception silently or leave empty
             }
