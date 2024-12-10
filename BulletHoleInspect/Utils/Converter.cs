0a086f1bb682afdc0c826b25fee5bda30788684d
alexinabox <main@alexinabox.de>
2024-12-10 13:37:51 +0100

    got too freaky with the literals

--- BulletHoleInspect/Utils/Converter.cs
+++ BulletHoleInspect/Utils/Converter.cs
@@ -23,7 +23,7 @@ namespace BulletHoleInspect.Utils
                         .OutputToFile(oggFilePath, options => options
                             .WithAudioCodec("libvorbis")
                             .WithAudioSamplingRate(48000)
-                            .WithAudioFilters(filter => filter.Pan("mono", "c0 < 0.9 * c0 + 0.1 * c1")))
+                            .WithAudioFilters(filter => filter.Pan("mono", "c0=c0")))
                         .ProcessSynchronously();
 
                     Log.Info($"Successfully converted the file to: {oggFilePath}");
