f99d8a3a6c00fe51588005bf4fdbd75467e63f55
alexinabox <main@alexinabox.de>
2024-12-10 10:41:16 +0100

    not a task

--- BulletHoleInspect/Utils/ElevenlabsWrapper.cs
+++ BulletHoleInspect/Utils/ElevenlabsWrapper.cs
@@ -4,7 +4,7 @@ namespace BulletHoleInspect.Utils
 
     internal sealed class ElevenlabsWrapper
     {
-        public static async Task GenerateVoiceline(string text)
+        public static async GenerateVoiceline(string text)
         {
             Log.Info(text);
             // Add your implementation here
