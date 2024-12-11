e21b606d78eb7dfa6a5e487fcaa3bc57924a6de2
alexinabox <main@alexinabox.de>
2024-12-11 10:11:20 +0100

    change default voice and model to a cheaper and more stable voice

--- ArtificialCassie/Config.cs
+++ ArtificialCassie/Config.cs
@@ -22,8 +22,8 @@ namespace ArtificialCassie
 
         [Description("Generate an API Key at: https://elevenlabs.io/app/settings/api-keys")]
         public string elevenlabs_api_key { get; set; }
-        public string voice_id { get; set; } = "21m00Tcm4TlvDq8ikWAM";
-        public string model_id { get; set; } = "eleven_multilingual_v2";
+        public string voice_id { get; set; } = "tTZ0TVc9Q1bbWngiduLK";
+        public string model_id { get; set; } = "eleven_turbo_v2_5";
 
         [Description("I recommend to leave this on. This will reuse previously generated voicelines to save YOU money and tokens!")]
         public bool ReuseVoicelines { get; set; } = true;
