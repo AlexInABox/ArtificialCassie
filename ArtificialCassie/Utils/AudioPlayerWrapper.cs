5de5e673870209668ea251ffe8823d2a62d76b32
Alexander Betke <main@alexinabox.de>
2024-12-14 00:06:17 +0100

    import fakeconnectionlist

--- ArtificialCassie/Utils/AudioPlayerWrapper.cs
+++ ArtificialCassie/Utils/AudioPlayerWrapper.cs
@@ -7,6 +7,7 @@ namespace ArtificialCassie.Utils
     using Exiled.Events.EventArgs;
     using Exiled.Events.EventArgs.Player;
     using AudioPlayer.API;
+    using AudioPlayer.Other;
     using static AudioPlayer.Plugin;
     using PlayerRoles;
     using System;
